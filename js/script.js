3e99538577153891b6ab281340e60accecfff0fc
alindanguyen <75220026+alindanguyen@users.noreply.github.com>
2021-02-07 14:39:22 -0800

    Fix syntax for height from = to :

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let pokemonList = [
   {name: 'Zubat', height: '0.8m', type: ['poison','flying']},
-  {name: 'Pidgey', height='0.3m', type: ['flying','normal']},
-  {name: 'Oddish', height='0.5m', type: ['grass','poison']},
+  {name: 'Pidgey', height: '0.3m', type: ['flying','normal']},
+  {name: 'Oddish', height: '0.5m', type: ['grass','poison']},
 ];
