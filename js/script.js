f5a3b65c6ba8e04d01cba857032b23b01ed1ac25
alindanguyen <75220026+alindanguyen@users.noreply.github.com>
2021-02-07 14:34:25 -0800

    Defining an array for the variable created: pokemonList

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert('Hello word');
-
-let favoriteFood = 'burritos';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Zubat', height: '0.8m', type: ['poison','flying']},
+  {name: 'Pidgey', height='0.3m', type: ['flying','normal']},
+  {name: 'Oddish', height='0.5m', type: ['grass','poison']},
+];
